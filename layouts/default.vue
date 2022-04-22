1b5a0440a7581b982a6b490102de5fcf245c519d
Linus Sundman <linus.sundman@abbgymnasiet.se>
2022-04-22 20:13:32 +0200

    Fixade hea

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,6 +7,17 @@
 </template>
 
 <script>
-import Head from "../components/NavBar.vue"
-import Foot from "../components/Footer.vue"
+  import Head from "../components/NavBar.vue"
+  import Foot from "../components/Footer.vue"
+
+  export default{
+    components: {
+      Head,
+      Foot
+
+    }
+  }
+
+
+
 </script>
